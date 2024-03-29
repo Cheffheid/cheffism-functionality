45841577da842d2b41b304bc503f33d4dc097e40
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-29 16:45:38 -0400

    Fix labels to be translateable

--- admin/partials/project-metabox.php
+++ admin/partials/project-metabox.php
@@ -12,10 +12,13 @@
 ?>
 <p>
 	<label for="cheffism_in_progress">
-		<input type="checkbox" name="cheffism_in_progress" id="cheffism_in_progress" <?php checked( 'on', $cheffism_in_progress[0] ); ?> /> In Progress?
+		<input type="checkbox" name="cheffism_in_progress" id="cheffism_in_progress" <?php checked( 'on', $cheffism_in_progress ); ?> />
+		<?php esc_html_e( 'In Progress?', 'cheffism' ); ?>
 	</label>
 </p>
 <p>
-	<label for="cheffism_project_link">Link</label>
-	<input name="cheffism_project_link" id="cheffism_project_link" type="text" value="<?php echo esc_attr( $cheffism_project_link[0] ); ?>" /><br />
+	<label for="cheffism_project_link">
+		<?php esc_html_e( 'Link', 'cheffism' ); ?>
+	</label>
+	<input name="cheffism_project_link" id="cheffism_project_link" type="text" value="<?php echo esc_attr( $cheffism_project_link ); ?>" /><br />
 </p>
